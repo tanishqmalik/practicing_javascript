c9f68aa3584e58a66591674359f1cfa3e918167c
tanishq <tanishq.malik31@gmail.com>
2024-02-26 18:25:56 +0530

    okay

---
+++ class/script.js
@@ -0,0 +1,8 @@
+
+const paragraph= document.getElementById('clickme')
+
+
+paragraph.addEventListener('click', ()=>{
+    var paragraph1= document.getElementById('hello')
+    paragraph1.style.color='red'
+})
