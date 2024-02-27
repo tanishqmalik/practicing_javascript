15d7e8776231d04e13cac0282fcee3749dbdeabe
tanishq <tanishq.malik31@gmail.com>
2024-02-27 15:47:12 +0530

    class

--- class/script.js
+++ class/script.js
@@ -6,3 +6,18 @@ paragraph.addEventListener('click', ()=>{
     var paragraph1= document.getElementById('hello')
     paragraph1.style.color='red'
 })
+
+
+const imagecontainer= document.getElementById('imagecontainer');
+const imgageone = document.getElementById('img1');
+const imagetwo= document.getElementById('img2');
+
+imagecontainer.addEventListener("mouseenter", ()=>{
+    imgageone.style.display='none'
+    imagetwo.style.display='inline-block'
+})
+
+imagecontainer.addEventListener("mouseleave", ()=>{
+    imagetwo.style.display="none"
+    imgageone.style.display="inline-block"
+})
